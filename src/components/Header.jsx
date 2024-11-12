97a1e4b831149c23d86d430b8a7e6d770b58b41b
kogilo <getabullaothow@gmail.com>
2024-11-12 01:39:40 -0600

    First commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import  quiz_app_logo from "../assets/quiz_app_logo.png"
+
+export default function Header() {
+    return <header>
+        <img src={quiz_app_logo} alt="Quiz logo" />
+        <h1>Quiz App</h1>
+
+    </header>
+};
