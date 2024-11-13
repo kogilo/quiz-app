32410bd2b2ce244701277cb928be4ed1fecaff29
kogilo <getabullaothow@gmail.com>
2024-11-12 20:47:56 -0600

    Questions and answers displayed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import { useState } from 'react'
-import Header from './components/Header'
+import Header from './components/Header.jsx'
+import Quiz from './components/Quiz.jsx'
 
 // import './App.css'
 
 function App() {
-  return <Header />
+  return (
+  <>
+  <Header />
+  <main>
+    <Quiz />
+  </main>
+  
+  </>
+  );
 }
 
 export default App
